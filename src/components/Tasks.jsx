b4d1c7fbf206e4f0170ff4fec7bec2be85db38d1
Niem Nguyen <luuniem87@gmail.com>
2020-01-22 01:30:49 -0800

    this adds fetch method and utilizing Firebase to POST data. Still need to be able to load data once user enters page

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -5,10 +5,20 @@ import TaskList from "./TaskList";
 const Tasks = () => {
   const [addedTasks, setAddedTasks] = useState([]);
   const addTaskHandler = task => {
-    setAddedTasks(prevTasks => [
-      ...prevTasks,
-      { id: Math.random().toString(), ...task }
-    ]);
+    fetch("https://to-do-list-v2.firebaseio.com/tasks.json", {
+      method: "POST",
+      body: JSON.stringify(task),
+      headers: { "Content-Type": "application/json" }
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(responseData => {
+        setAddedTasks(prevTasks => [
+          ...prevTasks,
+          { id: responseData.name, ...task }
+        ]);
+      });
   };
   const removeTaskHandler = taskId => {
     setAddedTasks(prevTasks => prevTasks.filter(task => task.id !== taskId));
